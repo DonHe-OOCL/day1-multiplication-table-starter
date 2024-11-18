eb9558f5ce8e9d5bfbfc42f6c8d71b9a29cadc86
HEDO <hedo@oocl.com>
2024-11-18 17:37:22 +0800

    [multiplicationTable]feat: build multiplication table

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -1,19 +1,26 @@
 package com.tw;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiplicationTableBuilder {
 
     public static void main(String[] args) {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
         int start = 2;
         int end = 4;
-        String multiplicationTable = builder.build(start, end);
+        String multiplicationTable = builder.generateMultiplicationTable(start, end);
 
         System.out.println(multiplicationTable);
     }
 
-    public String build(int start, int end) {
-        return "";
+    public static String generateMultiplicationTable(int startNumber, int endNumber) {
+        if (!isValid(startNumber, endNumber)) {
+            return null;
+        }
+        return buildMultiplicationTable(startNumber, endNumber);
     }
+
 
     public static boolean isValidInputOnRange(int number) {
         return number > 1 && number <= 1000;
@@ -32,8 +39,20 @@ public class MultiplicationTableBuilder {
         for (int i = startNumber; i <= endNumber; i++) {
             builder.append(i).append("*").append(endNumber).append("=").append(i * endNumber);
             if (i != endNumber) {
-                builder.append("\t");
+                builder.append(" ");
             }
+        }
+        return builder.toString();
+    }
+
+    public static String buildMultiplicationTable(int startNumber, int endNumber) {
+        List<String> rows = new ArrayList<>();
+        for (int i = startNumber; i <= endNumber; i++) {
+            rows.add(buildMultiplicationRow(startNumber, i));
+        }
+        StringBuilder builder = new StringBuilder();
+        for (String row : rows) {
+            builder.append(row).append("\r\n");
         }
         return builder.toString();
     }
